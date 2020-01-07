6d41b49815f9364895a3dc73e408a58307e013eb
Julian Kyer <julian.kyer@cognizant.com>
2020-01-07 00:37:25 -0700

    add gitignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f8c41419a94415bc571a";
+/******/ 	var hotCurrentHash = "885e297e18fd4d76729f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
