175e7017a741f8abf60bf26cd8c35ab971af9006
Julian Kyer <julian.kyer@cognizant.com>
2020-01-13 19:08:17 -0700

    add update item functionality

--- components/UpdateItem.js
+++ components/UpdateItem.js
@@ -19,20 +19,21 @@ const SINGLE_ITEM_QUERY = gql`
 
 const UPDATE_ITEM_MUTATION = gql`
   mutation UPDATE_ITEM_MUTATION(
-    $title: String!
-    $description: String!
-    $price: Int!
-    $image: String
-    $largeImage: String
+    $id: ID!
+    $title: String
+    $description: String
+    $price: Int
   ) {
     updateItem(
+      id: $id
       title: $title
       price: $price
       description: $description
-      image: $image
-      largeImage: $largeImage
     ) {
       id
+      title
+      description
+      price
     }
   }
 `;
@@ -47,24 +48,30 @@ class UpdateItem extends Component {
     this.setState({ [name]: val});
   }
 
+  updateItem = async (e, updateItemMutation) => {
+    e.preventDefault();
+    console.log('Updating Item');
+    console.log(this.state);
+    const res = await updateItemMutation({
+      variables: {
+        id: this.props.id,
+        ...this.state,
+      }
+    });
+    console.log('updated');
+    
+  }
+
   render() {
     return (
       <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
         {({data, loading}) => {
           if(loading) return <p>Loading...</p>;
+          if(!data.item) return <p>No item found for ID: ${this.props.id} :(</p>
           return (
             <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
               {(updateItem, { loading, error}) => (
-                <Form 
-                  onSubmit={async e => {
-                    e.preventDefault();
-                    const res = await updateItem();
-                    Router.push({
-                      pathname: '/item',
-                      query: { id: res.data.updateItem.id }
-                    })
-                  }}
-                >
+                <Form onSubmit={e => this.updateItem(e, updateItem)}>
                   <Error error={error} />
                   <fieldset disabled={loading} aria-busy={loading}>
                     <label htmlFor='title'>
@@ -101,7 +108,7 @@ class UpdateItem extends Component {
                         onChange={this.handleChange}
                       />
                     </label>
-                    <button type='Submit'>Submit</button>
+                    <button type='Submit'>Sav{loading ? `ing` : `e`} Changes</button>
                   </fieldset>
                 </Form>
               )}
