0eae817fb5bdfd4f1746bec5a5969a873e3dc585
Julian Kyer <julian.kyer@cognizant.com>
2020-01-13 19:33:09 -0700

    add delete item functionality

--- components/Item.js
+++ components/Item.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
+import DeleteItem from './DeleteItem';
+
 // import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 // import PriceTag from './styles/PriceTag';
+// move price tag stuff/format money func to single item view
 import formatMoney from '../lib/formatMoney';
 
 class Item extends Component {
@@ -34,7 +37,7 @@ class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add to cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
