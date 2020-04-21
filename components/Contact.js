ef9476c0f526ff255391418b671aad165670e032
Julian Kyer <julian.kyer@gmail.com>
2020-04-20 22:59:39 -0600

    add basic contact

--- components/Contact.js
+++ components/Contact.js
@@ -1,10 +1,23 @@
 import styled from "styled-components";
 
 const StyledContact = styled.div`
-  border: 2px solid magenta;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 5% 1fr 5%;
+  align-content: center;
+  margin-bottom: 40px;
+  width: 100%;
+
+  h1,
+  p,
+  a {
+    grid-column: 2/3;
+    justify-self: center;
+  }
+
+  a {
+    cursor: pointer;
+    color: ${(props) => props.theme.brand01};
+  }
 `;
 
 const Contact = () => (
@@ -12,11 +25,21 @@ const Contact = () => (
     <h1>Let's get started today</h1>
     <p>
       The best way to get your perfect ring or completely custom jewlery is to
-      call me
+      call or email me. Let's talk so that I can best understand your perfect
+      piece of jewelry.
     </p>
-    <a href="mailto:jenniferamydesigns@gmail.com" target="_blank">
-      jenniferamydesigns@gmail.com
-    </a>
+    <p>
+      Phone:{"  "}
+      <a className="footer__phone-number" href="tel: 949-784-9719">
+        949-784-9719
+      </a>
+    </p>
+    <p>
+      Email:{"  "}
+      <a href="mailto:jenniferamydesigns@gmail.com" target="_blank">
+        jenniferamydesigns@gmail.com
+      </a>
+    </p>
   </StyledContact>
 );
 
--- components/Process.js
+++ components/Process.js
@@ -65,23 +65,31 @@ const Process = () => {
       <div className="plx__img-wrapper plx__img-wrapper--first">
         <span
           className="backgound-image"
-          aria-label="image of blonde model with a glass of wine and diamond ring"
+          aria-label="blonde model with a glass of wine and diamond ring"
         ></span>
       </div>
 
       <div className="plx__content">
-        <h3>How We Create You Custom Jewelry, Together</h3>
-        <p className="quote">
+        <h3>Completely Custom</h3>
+        {/* <p className="quote">
           <i>
             "Maybe it's not about the happy ending. Maybe it's about the story.”
           </i>
+        </p> */}
+        <p>
+          The core value of Jennifer Amy Jewelry is to bestow the client with
+          the most elegant, suited, ring. This can mean variety of things
+          including color, cost, and needs.
         </p>
         <p>
-          Every experience leaves a memory. The defining moments in life are
-          what creates an ever lasting memory. Working with Jennifer Amy will
-          create the high end personal experience that everyone dreams for,
-          using her deep connections and hand selected pieces from a private
-          jeweler.
+          How does the process work you ask? It is actually quite simple. Start
+          with an idea of what you would like. Pick an example from a friend or
+          from a magazine. Once you have a foundation of what you would like,
+          give Jennifer a call.
+        </p>
+        <p>
+          The initial process doesn’t take long. It simply is a conversation to
+          fully allow Jennifer to understand your dream piece.
         </p>
       </div>
 
@@ -95,9 +103,21 @@ const Process = () => {
       <div className="plx__content">
         <h3>Just for You</h3>
         <p>
-          Every story is different and every journey is unique. Trust Jennifer
-          Amy Jewelry to help you create engagement and wedding rings to match
-          your one-of-a-kind love.
+          Next she will take her masterful experience and create various mockups
+          for you to enjoy. Settling on a ring isn’t easy. The process continues
+          with a bit more precision and accuracy on both sides by picking what
+          you love and what you can live without.
+        </p>
+        <p>
+          Next the gem will be placed in a ring and examined by Jennifer. Once
+          everything fits like a glove, Jennifer will let you experience your
+          new piece. The final step is crossing the t’s and dotting the I’s with
+          insurance and paperwork. Your new ring will be delivered to you before
+          your magical day.
+        </p>
+        <p>
+          No matter a wedding or a gift, Jennifer Amy Jewelry will create your
+          dream piece.
         </p>
       </div>
 
